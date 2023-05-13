d89a986afef3a94314585ae21a17f01449e7d289
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2023-05-13 22:12:56 +0530

    Update Myportfolio.js

---
+++ Myportfolio.js
@@ -0,0 +1,18 @@
+// Header Toggle
+
+let Menubtn = document.getElementById('Menubtn')
+
+Menubtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Java Developer !', 'UI/UX Designer !', 'Front-End-Developer !'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
